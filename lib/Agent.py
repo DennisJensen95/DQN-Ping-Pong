061895fe0bc8895ebdf172bbf22a6709e49b43f8
Dennis <ngh1adj95@hotmail.com>
2019-11-25 10:46:11 +0100

    It is actually learning now

--- lib/Agent.py
+++ lib/Agent.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from lib.ImageProcess import ImageProcessing
+import time
 
 class Agent():
     def __init__(self, env, memory):
@@ -15,13 +16,14 @@ class Agent():
         self.env_set = True
         self.total_reward = 0.0
 
-    def use_state(self, network, device):
+    def random_epsilon_greedy(self, network, device):
         assert (self.env_set), "Please initialize game before playing by resetting or inputting a state"
         state = np.array([self.env_state], copy=False)
         state = torch.tensor(state).to(device)
         q_values = network(state)
         _, pref_action = torch.max(q_values, dim=1)
-        action = pref_action.item()
+        action = int(pref_action.item())
+        # print(action)
         return action
 
     def play_action(self, network, e=0, device='cpu'):
@@ -29,8 +31,14 @@ class Agent():
         if np.random.uniform(0, 1) < e:
             # Taking random action
             action = self.env.action_space.sample()
+            # print(action)
+            # print("Totally random")
+            # time.sleep(1)
         else:
-            action = self.use_state(network, device)
+            action = self.random_epsilon_greedy(network, device)
+            # print(action)
+            # print("Epsilon random")
+            # time.sleep(1)
 
         # Do action in the environment
         new_state, reward, done, _ = self.env.step(action)
@@ -38,7 +46,7 @@ class Agent():
         old_state = self.env_state
         self.env_state = new_state
 
-        self.memory.save_to_memory(old_state, action, self.env_state, reward)
+        self.memory.save_to_memory(old_state, action, reward, done, self.env_state)
 
         if done:
             done_reward = self.total_reward
